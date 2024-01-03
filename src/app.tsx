5bf5cb13904a80ab30f25cf574b99067e3006615
叶枫 <645381995@qq.com>
2024-01-03 16:34:19 +0800

    feat: demo

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,7 @@
 // 运行时配置
 
+import { useModel } from '@umijs/max';
+
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
 export async function getInitialState(): Promise<{ name: string }> {
@@ -14,3 +16,14 @@ export const layout = () => {
     },
   };
 };
+
+const App = (props: { children?: React.Component }) => {
+  const user = useModel('global');
+  console.log('user', user);
+
+  return <>{props.children}</>;
+};
+
+export function rootContainer(container: React.Component) {
+  return <App>{container}</App>;
+}
