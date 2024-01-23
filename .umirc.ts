07cec66af652ffa3f0c96b42237281aa27d2b26b
叶枫 <645381995@qq.com>
2024-01-23 16:16:25 +0800

    feat: done

--- .umirc.ts
+++ .umirc.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from '@umijs/max';
 
 export default defineConfig({
-  antd: {
-    configProvider: { prefixCls: 'test' },
-    styleProvider: {
-      hashPriority: 'high',
-      legacyTransformer: true,
-    },
-  },
+  // antd: {
+  //   configProvider: { prefixCls: 'test' },
+  //   styleProvider: {
+  //     hashPriority: 'high',
+  //     legacyTransformer: true,
+  //   },
+  // },
   access: {},
   model: {},
   initialState: {},
   request: {},
-  layout: {
-    title: '@umijs/max',
-  },
+  // layout: {
+  //   title: '@umijs/max',
+  // },
   routes: [
     {
       path: '/',
@@ -24,16 +24,6 @@ export default defineConfig({
       name: '首页',
       path: '/home',
       component: './Home',
-    },
-    {
-      name: '权限演示',
-      path: '/access',
-      component: './Access',
-    },
-    {
-      name: ' CRUD 示例',
-      path: '/table',
-      component: './Table',
     },
   ],
   npmClient: 'pnpm',
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,41 +1,12 @@
-import Guide from '@/components/Guide';
-import { trim } from '@/utils/format';
-import { PageContainer } from '@ant-design/pro-components';
-import { useModel } from '@umijs/max';
-import { Button, Modal, Radio, Select } from 'antd';
-import styles from './index.less';
+import { Toast } from 'antd-mobile';
+
+console.log('Toast', Toast);
 
 const HomePage: React.FC = () => {
-  const { name } = useModel('global');
   return (
-    <PageContainer ghost>
-      <div className={styles.container}>
-        <Guide name={trim(name)} />
-        <Button
-          onClick={() => {
-            Modal.confirm({ title: 'aaa' });
-          }}
-        >
-          aaa
-        </Button>
-        <Radio.Group>
-          <Radio value={1}>A</Radio>
-          <Radio value={2}>B</Radio>
-          <Radio value={3}>C</Radio>
-          <Radio value={4}>D</Radio>
-        </Radio.Group>
-        <Select
-          defaultValue="lucy"
-          style={{ width: 120 }}
-          options={[
-            { value: 'jack', label: 'Jack' },
-            { value: 'lucy', label: 'Lucy' },
-            { value: 'Yiminghe', label: 'yiminghe' },
-            { value: 'disabled', label: 'Disabled', disabled: true },
-          ]}
-        />
-      </div>
-    </PageContainer>
+    <>
+      <a>aaaa</a>
+    </>
   );
 };
 
